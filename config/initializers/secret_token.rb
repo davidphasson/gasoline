fd425af50a347d2b40a1f01a94d40865cbc34140
David Hasson <dph@funtaff.net>
2012-03-27 17:51:42 -0700

    Rails 3.2.2 Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gasoline::Application.config.secret_token = 'fa7dcfa2b5ad096f65ee1f9cccaa26123da21854d800df5e99faa5550116791da3d4037d194491223a50be3d525aa98e673c4334bb25dd2889b217daa76bbf19'
